5a0532206e288dbadfb7514aaa0aaf925e3eebcf
Ludy-Amarillis-14 <pl0044032025@unab.edu.sv>
2026-04-10 21:55:33 -0600

    Código base del Proyecto de la Agenda de Conactos

---
+++ ContactManagerWeb/Controllers/ContactosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagerWeb.Controllers
+{
+    public class ContactosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
