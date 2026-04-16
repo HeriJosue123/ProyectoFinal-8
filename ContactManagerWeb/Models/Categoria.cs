9e4f6dba35a1d3f87a4d1f8cef4346822864d515
jenniruiz0820 <jennxfr.sr17@gmail.com>
2026-04-15 20:36:01 -0600

    Creación de entidad base Categoria

---
+++ ContactManagerWeb/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactManagerWeb.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string NombreCategoria { get; set; }
+        public string Descripcion { get; set; }
+        public string ColorHexadecimal { get; set; }
+    }
+}
