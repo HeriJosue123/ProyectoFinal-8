44522783e92eedf75d717313c826b212b55cb878
Yeyson Echevrria <yeysonecheverria410@gmail.com>
2026-04-15 20:45:35 -0600

    Atributos de negocio en Contacto y nuevos endpoints en Controlador

--- ContactManagerWeb/Models/Contacto.cs
+++ ContactManagerWeb/Models/Contacto.cs
@@ -1,4 +1,6 @@
-﻿namespace ContactManagerWeb.Models
+﻿using System;
+
+namespace ContactManagerWeb.Models
 {
     public class Contacto
     {
@@ -6,5 +8,11 @@
         public string Nombre { get; set; }
         public string Telefono { get; set; }
         public string Correo { get; set; }
+
+
+        public string Direccion { get; set; }
+        public string Categoria { get; set; }
+        public bool EsFavorito { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
     }
 }
