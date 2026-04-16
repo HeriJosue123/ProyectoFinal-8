d4a5fd3b4fabbf1f0668a1d61937fe9211ebe92b
josue <heribertojosue123@gmail.com>
2026-04-15 20:42:57 -0600

    Creación de entidad principal Contacto con datos personales

--- ContactManagerWeb/Models/Contacto.cs
+++ ContactManagerWeb/Models/Contacto.cs
@@ -2,11 +2,9 @@
 {
     public class Contacto
     {
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Telefono { get; set; }
         public string Correo { get; set; }
-        public string Direccion { get; set; }
-        public string Categoria { get; set; }
-        public bool EsFavorito { get; set; }
     }
 }
